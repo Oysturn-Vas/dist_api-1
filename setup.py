4558009db9c4d322a4da6f4b1fb5a1163b5a3d3a
Oysturn Vas <oysturnxvas@gmail.com>
2022-08-26 14:23:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,20 @@ import numpy as np
 from fastapi import FastAPI
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 cred = credentials.Certificate(
